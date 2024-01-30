dd70d30307f37a9cf61d81b4dcd8e1c47b705cc1
KangJeoungMi <kbg973855@gmail.com>
2024-01-30 17:22:37 +0900

    upload

---
+++ part1/src/ch01/scanner.java
@@ -0,0 +1,20 @@
+package ch01;
+
+import java.util.Scanner;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("이름을 입력하세요");
+		String str = scanner.nextLine();
+		
+		
+		System.out.println("나이를 입력하세요");
+		String age = scanner.nextLine();
+		int y = Integer.parseInt(age);
+		
+	}
+	
+}
