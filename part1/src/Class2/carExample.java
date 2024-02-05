9ebbec42989b13b7aafe424ebd5aaddbeaab349c
KangJeoungMi <kbg973855@gmail.com>
2024-02-05 09:47:14 +0900

    upload

---
+++ part1/src/Class2/carExample.java
@@ -0,0 +1,14 @@
+package Class2;
+
+
+
+public class carExample {
+	public static void mauib(String[] args) {
+		Car myCAR = new Car();
+		
+		System.out.println(myCAR.model);
+		System.out.println(myCAR.start);
+		System.out.print(myCAR.speed);
+	}
+
+}
